36ac9641835e23e37b2f912d39db738bdda7a51c
Rohit Surve <rohsurve@umail.iu.edu>
2016-09-14 23:12:48 -0400

    rest jersey

--- src/com/services/QuestionareService.java
+++ src/com/services/QuestionareService.java
@@ -3,9 +3,10 @@
  */
 package com.services;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -16,22 +17,42 @@ import javax.ws.rs.core.Response;
 public class QuestionareService {
 
 	@GET
-	@Path("/{param}")
-	public Response answerQns(@PathParam("param") String question) {
+	public Response answerQns(@QueryParam("q") @DefaultValue("No answer")String question) {
+		String ans = "Please provide Correct question!!";
 
-		System.out.println(question);
-		String answer = "I dont Know!!!";
+		if ("What is your full name?".equalsIgnoreCase(question)) {
+			ans = "Rohit Surve";
+		} else if ("What is you email address?".equalsIgnoreCase(question)) {
+			ans = "rohsurve@iu.edu";
+		} else if ("Please provide a phone number we can use to reach you?".equalsIgnoreCase(question)) {
+			ans = "8123498667";
+		} else if ("Which position at Mov-ology are you applying for?".equalsIgnoreCase(question)) {
+			ans = "Sr.Software Engineer";
 
-		return Response.status(200).entity(answer).build();
+		} else if ("How many years of software development experience do you have?".equalsIgnoreCase(question)) {
+			ans = "7 years";
 
-	}
-	
-	@GET
-	public Response defaultQnAns() {
+		} else if ("Please provide a URL where we can download your resume and cover letter."
+				.equalsIgnoreCase(question)) {
+			ans = "rohitsurve.strikingly.com";
 
-		String answer = "Default answer";
+		} else if ("Which position at Mov-ology are you applying for?".equalsIgnoreCase(question)) {
+			ans = "Sr.Software Engineer";
 
-		return Response.status(200).entity(answer).build();
+		} else if ("How many years of software development experience do you have?".equalsIgnoreCase(question)) {
+			ans = "7 years";
+
+		} else if ("Please provide a URL where we can download the source code of your resume submission web service."
+				.equalsIgnoreCase(question)) {
+			ans = "https://github.com/rohsurve/movologyservice";
+		} else if ("Can you provide proof of eligibility to work in the US?".equalsIgnoreCase(question)) {
+			ans = "Yes";
+
+		} else if ("Please solve this puzzle:".equalsIgnoreCase(question)) {
+			ans = "Can't answer this as question .Dont know what is the puzzle";
+		}
+
+		return Response.status(200).entity(ans).build();
 
 	}
 
