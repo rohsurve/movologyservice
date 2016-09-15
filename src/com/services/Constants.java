86c019e0bff65d5ad723627e310ce955dd300e85
Rohit Surve <rohsurve@umail.iu.edu>
2016-09-15 03:28:26 -0400

    added questionaire service

---
+++ src/com/services/Constants.java
@@ -0,0 +1,20 @@
+package com.services;
+
+public class Constants {
+
+	private Constants() {
+
+	}
+	public final static String PING = "['q']['Ping']";
+	public final static String NAME = "['q']['Name']";
+	public final static String EMAIL = "['q']['Email']";
+	public final static String PHONE = "['q']['Phone']";
+	public final static String POSITION = "['q']['Position']";
+	public final static String YEARS = "['q']['Years']";
+	public final static String RESUME = "['q']['Resume']";
+	public final static String REFERRER = "['q']['Referrer']";
+	public final static String SOURCE = "['q']['Source']";
+	public final static String STATUS = "['q']['Status']";
+	public final static String PUZZLE = "['q']['Puzzle']";
+
+}
--- src/com/services/QuestionareService.java
+++ src/com/services/QuestionareService.java
@@ -16,82 +16,73 @@ import javax.ws.rs.core.Response;
 
 /**
  * @author rohsurve
+ * 
+ * Service is responsible for returning answers for the input questions.
  *
  */
 @Path("/questionsService")
 public class QuestionareService {
 
+	/**
+	 * Default response
+	 * 
+	 * @return response
+	 */
+	@GET
+	public Response defaultGet() {
+		return Response.status(200).entity("Questionare Rest Service works!!").build();
+	}
+
+	/**
+	 * Api to get answers for the input question.
+	 * 
+	 * @param q
+	 * @param puzzle
+	 * @return response
+	 */
 
 	@POST
-	public Response testPost(@QueryParam("q") String q, @QueryParam("puzzle") String puzzle) {
-		
-		String ans = "No answer for this q!!";
+	public Response answerQns(@QueryParam("q") String q, @QueryParam("puzzle") String puzzle) {
 
-		if("['q']['Ping']".equalsIgnoreCase(q)){
+		String ans = "";
+
+		if (Constants.PING.equalsIgnoreCase(q)) {
 			ans = "OK";
-		}
-		else if ("['q']['Name']".equalsIgnoreCase(q)) {
+		} else if (Constants.NAME.equalsIgnoreCase(q)) {
 			ans = "Rohit Surve";
-		}
-		else if ("['q']['Email']".equalsIgnoreCase(q)) {
+		} else if (Constants.EMAIL.equalsIgnoreCase(q)) {
 			ans = "rohsurve@iu.edu";
-		}
-		else if ("['q']['Phone']".equalsIgnoreCase(q)) {
+		} else if (Constants.PHONE.equalsIgnoreCase(q)) {
 			ans = "8123498667";
-		}
-		else if ("['q']['Position']".equalsIgnoreCase(q)) {
+		} else if (Constants.POSITION.equalsIgnoreCase(q)) {
 			ans = "Sr.Software Engineer";
-
-		}
-		else if ("['q']['Years']".equalsIgnoreCase(q)) {
+		} else if (Constants.YEARS.equalsIgnoreCase(q)) {
 			ans = "7 years";
-
-		}
-		else if ("['q']['Resume']".equalsIgnoreCase(q)) {
+		} else if (Constants.RESUME.equalsIgnoreCase(q)) {
 			ans = "rohitsurve.strikingly.com";
-
-		}
-		else if ("['q']['Referrer']".equalsIgnoreCase(q)) {
+		} else if (Constants.REFERRER.equalsIgnoreCase(q)) {
 			ans = "Indeed.com";
-
-		}
-		else if ("['q']['Name']".equalsIgnoreCase(q)) {
-			ans = "7 years";
-
-		}
-		else if ("['q']['Source']"
-				.equalsIgnoreCase(q)) {
+		} else if (Constants.SOURCE.equalsIgnoreCase(q)) {
 			ans = "https://github.com/rohsurve/movologyservice";
-		}
-		else if ("['q']['Status']".equalsIgnoreCase(q)) {
+		} else if (Constants.STATUS.equalsIgnoreCase(q)) {
 			ans = "Yes";
+		} else if (Constants.PUZZLE.equalsIgnoreCase(q)) {
 
-		}
-		else if ("['q']['Puzzle']".equalsIgnoreCase(q)) {
-			
 			byte[] decoded = Base64.getDecoder().decode(puzzle);
-			System.out.println(new String(decoded));
-			
 			ScriptEngineManager manager = new ScriptEngineManager();
-			ScriptEngine engine = manager.getEngineByName("js");  
-			Integer result=null;
+			ScriptEngine engine = manager.getEngineByName("js");
+			Integer result = null;
 			try {
-				 result = (Integer)engine.eval(new String(decoded));
+				result = (Integer) engine.eval(new String(decoded));
+				ans = result.toString();
 			} catch (ScriptException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				return Response.status(500).entity(e.getMessage()).build();
 			}
-			
-			ans = result.toString();
+		} else {
+			ans = "No answer found !!";
 		}
 
 		return Response.status(200).entity(ans).build();
-		
-	}
-
-	@GET
-	public Response answerQns() {
-		return Response.status(200).entity("Rest Service Works").build();
 
 	}
 
